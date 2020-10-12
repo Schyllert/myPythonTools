440f076203496e4b441ff2feb4cfe969333bacc8
Eric Schyllert <mat11esc@student.lu.se>
2020-10-12 22:09:10 +0200

    Randomly leker lite

---
+++ learnPython.py
@@ -0,0 +1,18 @@
+print("yoyoyoyoyoyoyooyoy")
+
+kuken = "fitta"
+
+print(kuken)
+
+
+#color = input("what? ")
+
+fula_ord = ["kuken", "fitta", "hora"]
+fina_ord = ["Söt", "finis"]
+
+fula_ord.extend(fina_ord)
+
+print(fula_ord)
+print(fula_ord.pop())
+print(fula_ord)
+fula_ord.length()
